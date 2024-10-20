73661d62f6d2683165ec854b90c3768422cdbbec
Prudvi Kuamar Reddy NS <22102A030231@mbu.asia>
2024-10-20 10:27:17 +0530

    initial commit

--- api/main.py
+++ api/main.py
@@ -49,5 +49,5 @@ def activities(input: ActivitiesInput):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  
+    port = int(os.getenv("PORT", 10000))  
     uvicorn.run(app, host="0.0.0.0", port=port)
